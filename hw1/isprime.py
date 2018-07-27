c9a3b4e265ab1f4a464dd7d21edf7dc05bb1c4b0
Betulkars <betulkars@hacettepe.edu.tr>
2018-07-28 01:33:23 +0300

    son düzeltme

--- hw1/isprime.py
+++ hw1/isprime.py
@@ -1,15 +1,19 @@
 def asalMi(sayi):
     tamBolenler = []
-    for i in range(2, sayi):
+    for i in range(2, int(int(sayi) // 2) + 1):
         if (sayi % i == 0):
             tamBolenler.append(i)
-    return tamBolenler
+            return tamBolenler
 
-sayi = int(input("Sayi giriniz:"))
-tamBolenler=asalMi(sayi)
-if (sayi==1):
-    print("En küçük asal sayı 2 dir.Tekrar deneyiniz.")
-elif len(tamBolenler)==0:
-    print("Asal Sayıdır")
-else:
-    print("Asal Sayı Değildir, Tam Bölenler:", tamBolenler)
+try:
+    sayi = int(input("Sayi giriniz:"))
+    tamBolenler = asalMi(sayi)
+    if (sayi == 1):
+        print("En küçük asal sayı 2 dir.Tekrar deneyiniz.")
+    elif len(tamBolenler) == 0:
+         print("Asal Sayıdır")
+    else:
+        print("Asal Sayı Değildir, Tam Bölenler:", tamBolenler)
+except ValueError:
+    print("Lütfen pozitif tamsayı giriniz.")
+
