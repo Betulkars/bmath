52006ea3573c800ab5fb75258c27296102c72b9d
Betül Kars <36736169+Betulkars@users.noreply.github.com>
2018-07-31 22:36:37 +0300

    Update password.py

--- password.py
+++ password.py
@@ -4,14 +4,17 @@ if not password:
 elif len(password)<8 or len(password)>32:
     print("Parola uzunluğu 8 karakterden küçük ve 32 karakterden büyük olamaz!")
 else:
-    sayıvarmi=True
     terstrnakişrtvarmi=False
     tektrnkişrtvarmi=False
     tırnakiştvarmi=False
     boslukvarmi=False
-    zorunlukrkyokmu=True
-    küçükharfvarmi=True
-    büyükharfvarmi=True
+    zorunlukrkyokmu=False
+    büyükharfyokmu=True
+    küçükharfyokmu=True
+    sayiyokmu=True
+    # k = 0
+    # b = 0
+    # c = 0
     for karakter in password:
         if karakter == ' ':
             boslukvarmi=True
@@ -22,16 +25,19 @@ else:
         if karakter == "`":
             terstrnakişrtvarmi=True
         if karakter != "!" or karakter !="@" or karakter!=  "#"  or karakter != "$" or karakter != "%" or karakter!= "^" or karakter!= "&":
-            zorunlukrkyokmu=False
-        if karakter.islower()==False:
-            print("En az bir küçük harf bulundurunuz.")
-            break
-        if karakter.isupper()==False:
-            print("En az bir büyük harf bulundurunuz.")
-            break
-        if karakter.isnumeric()==False:
-            print("En az bir sayı bulundurunuz.")
-            break
+            zorunlukrkyokmu=True
+        if karakter.islower():
+            büyükharfyokmu=False
+            # k+=1
+        if karakter.isupper():
+            küçükharfyokmu=False
+
+            # b+=1
+        if karakter.isnumeric():
+            sayiyokmu=False
+            # c+=1
+
+
     if boslukvarmi:
         print("Parolanızda boşluk olamaz.")
     if tırnakiştvarmi:
@@ -42,7 +48,10 @@ else:
         print("Parolanızda ters tırnak işareti bulunamaz.")
     if zorunlukrkyokmu:
         print("Parolanızda !@#$%^& karakterlerinden birini bulundurunuz.")
-
-
-
+    if büyükharfyokmu:
+        print("En az bir küçük harfkullanın.")
+    if küçükharfyokmu:
+        print("En az bir büyük harf kullanın")
+    if sayiyokmu:
+        print("En az bir sayı kullanın")
 
