cdfaa992956ee2e4418672ec80f5f7a2a63c5623
Betulkars <betulkars@hacettepe.edu.tr>
2018-08-04 20:30:00 +0300

    ödev3düzeltme

--- hw1/matrix.py
+++ hw1/matrix.py
@@ -1,7 +1,14 @@
 m1=[[1,2,3],[4,5,6],[7,8,9]]
 m2=[[9,8,7],[6,5,4],[3,2,1]]
-m3=[[0,0,0],[0,0,0],[0,0,0]]
+m3=[]
 for i in range(3):
+    k_satir=list()
     for j in range(3):
-        m3[i][j]=m1[i][j]+m2[i][j]
-    print(m3[i])
+        m1_sayi=m1[i][j]
+        m2_sayi=m2[i][j]
+        toplam=m1_sayi+m2_sayi
+
+
+        k_satir.append(toplam)
+    m3.append(k_satir)
+print(m3)
