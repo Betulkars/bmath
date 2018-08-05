cfbe828d4ba3193d8a98d0d61453bbd97f68cd7a
Betulkars <betulkars@hacettepe.edu.tr>
2018-08-06 01:58:34 +0300

    düzeltme

--- hw1/matrix.py
+++ hw1/matrix.py
@@ -4,9 +4,7 @@ m3=[]
 for i in range(3):
     m_satir=[]
     for j in range(3):
-        m1[i][j]+m2[i][j]
-
-
+        # m1[i][j]+m2[i][j]
         m_satir.append(m1[i][j]+m2[i][j])
     m3.append(m_satir)
 print(m3)
