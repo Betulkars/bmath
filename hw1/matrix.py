5719bb9acb9a8eedabeeb1eb35f16e32dc875438
İbrahim Ethem Demirci <dmrc_wr@hotmail.com>
2018-08-15 23:59:26 +0300

    matrix multiply

--- hw1/matrix.py
+++ hw1/matrix.py
@@ -1,14 +1,16 @@
-m1=[[12, 7, 3],
-     [4, 5, 6],
-     [7, 8, 9]]
+m1=[[1, 0, 0],
+     [0, 1, 0],
+     [0, 0, 1]]
 m2=[[5, 8, 1, 2],
      [6, 7, 3, 0],
      [4, 5, 9, 1]]
 m3=[]
 for i in range(len(m1)):
-   for j in range(len(m2[0])):
-       for k in range(len(m2)):
-           m3[i][j]+=m1[i][k]+m2[k][j]
-
-for s in m3:
-    print(s)
+    pivot=[]
+    for j in range(len(m2[0])):
+        toplam=0
+        for k in range(len(m2)):
+            toplam+=m1[i][k]*m2[k][j]
+        pivot.append(toplam)
+    m3.append(pivot)
+print(m3)
